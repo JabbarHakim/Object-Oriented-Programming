715b1b153f48a6df9e86a4d2a829641b1f187227
JabbarHakim <jabbarhakimm@gmail.com>
2025-08-26 16:54:27 +0700

    Add files via upload

---
+++ Object_Oriented_Programming_I.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.ler;
+
+/**
+ *
+ * @author acer
+ */
+public class Object_Oriented_Programming_I {
+    
+    public static void main(String[] args) {
+        System.out.println("Nama : Jabbar Hakim");
+        System.out.println("NPM : 2440506074");
+        System.out.println("Alamat : Magelang");
+        System.out.println("No_HP : 08123123123");
+    }
+}
